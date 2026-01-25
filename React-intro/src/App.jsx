7d75f2604492c992e69b88a1635f3c5d1bfa2624
AdrianNebelung <acnebelu@hiof.no>
2026-01-25 17:27:27 +0100

    Øvningsoppgave og forlesings kode

---
+++ React-intro/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Title from "./components/Title";
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Title/>
+      <h2>Dette er kjempegøy</h2>
+    </>
+  )
+}
+
+export default App
--- øvningsoppgaver/forkunnskaper/courses.js
+++ øvningsoppgaver/forkunnskaper/courses.js
@@ -1,17 +1,17 @@
 const courses = [
     {
-        tittel: "Webutvikling",
+        tittel: "Les mer om HTML og CSS",
         beskrivelse: "Lær grunnsteiene i webutvikling med teknologiene HTML og CSS.",
         url: "https://www.hiof.no/studier/emner/iio/itk/2025/host/itf10511.html"
     },
     {
-        tittel: "JavaScript",
+        tittel: "Les mer on JavaScript",
         beskrivelse: "Lær et av de mest brukte scriptspråkene for frontend-design og utvikling.",
-        url: "javascript"
+        url: "https://www.w3schools.com/js/"
     },
     {
-        tittel: "React + Sanity",
+        tittel: "Les mer om React + Sanity",
         beskrivelse: "Lær avansert remmeverk og CMS-løsninger for applikasjonsutvikling.",
-        url: "reactsanity"
+        url: "https://www.sanity.io/exchange/framework=react"
     }
 ];
